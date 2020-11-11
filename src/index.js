b949fc5802169ef9c3b2de1988ac7ed5bbf4bf0d
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-11-11 18:03:39 -0300

    Add destructuring API DATA.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,51 @@ const APIKEY = '49eaa4abf6e4f3d9db8f20bfed37ffca';
 
 // const WEBAPI = `api.openweathermap.org/data/2.5/weather?q=${CITYNAME}&appid=${APIKEY}`;
 
-async function getWeather(CITYNAME, APIKEY) {
+const getWeather = async (CITYNAME, APIKEY) => {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITYNAME}&appid=${APIKEY}`);
-    const weatherData = await response.json();
-    console.log(weatherData);
+    const APIDATA = await response.json();
+    renderWeatherCard(APIDATA);
+    console.log(APIDATA);
+    return APIDATA;
   }
   catch (err) {
     console.log(err);
   }
 }
 
-getWeather('Cordoba', APIKEY);
+const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputValue = document.getElementById('search-input').value;
+  getWeather(inputValue, APIKEY);
+});
+
+const renderWeatherCard = (APIDATA) => {
+  const [temperatures, cityName, countryName, weather, wind] = [APIDATA.main, APIDATA.name, APIDATA.sys.country, APIDATA.weather[0], APIDATA.wind];
+  
+}
+
+// {coord: {…}, weather: Array(1), base: "stations", main: {…}, visibility: 10000, …}
+// base: "stations"
+// clouds: {all: 10}
+// cod: 200
+// coord: {lon: -60.7, lat: -31.63}
+// dt: 1605125678
+// id: 3836277
+// main: {temp: 303.51, feels_like: 302.64, temp_min: 302.59, temp_max: 304.26, pressure: 1009, …}
+// name: "Santa Fe"
+// sys: {type: 3, id: 2008823, country: "AR", sunrise: 1605085064, sunset: 1605134171}
+// timezone: -10800
+// visibility: 10000
+// weather: Array(1)
+// 0:
+// description: "clear sky"
+// icon: "01d"
+// id: 800
+// main: "Clear"
+// __proto__: Object
+// length: 1
+// __proto__: Array(0)
+// wind: {speed: 2.24, deg: 17, gust: 3.58}
+// __proto__: Object
