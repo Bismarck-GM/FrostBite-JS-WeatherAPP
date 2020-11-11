b40a503fc4cd86ac6eb20de37b8e98cb17cd3270
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-11-11 12:53:59 -0300

    Add webpack configuration. Add npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,12 +8,50 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+          publicPath: './images',
+        },
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
       title: 'Winter is comming',
       template: './src/index.html',
-      filename: './dist/index.html' //relative to root of the application
+      filename: './dist/index.html', // relative to root of the application
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
     }),
   ],
 };
